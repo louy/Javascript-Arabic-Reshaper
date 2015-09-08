a9b77b276eaaf2d834076753d39407d1ca2f0b5e
Louy <me@l0uy.com>
2015-09-08 23:32:48 +0300

    Simple test cases

--- test/index.js
+++ test/index.js
@@ -6,6 +6,21 @@ describe('ArabicReshaper', function() {
 
   var a = 'السلام عليكم', b = 'ﺍﻟﺴﻼﻡ ﻋﻠﻴﻜﻢ';
 
+  it('should convert an isolated letter', function() {
+  	var a = 'س', b = String.fromCharCode(0xFEB1);
+    expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+  });
+
+  it('should convert two letter', function() {
+  	var a = 'سس', b = String.fromCharCode(0xFEB3)+String.fromCharCode(0xFEB2);
+    expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+  });
+
+  it('should convert three letter', function() {
+  	var a = 'سسس', b = String.fromCharCode(0xFEB3)+String.fromCharCode(0xFEB4)+String.fromCharCode(0xFEB2);
+    expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+  });
+
   it('should convert', function() {
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
   });
