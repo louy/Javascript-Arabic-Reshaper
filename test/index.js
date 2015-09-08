587875d3d462781afebfbe5df942eb0f4765d0e3
Louy <me@l0uy.com>
2015-09-08 23:58:15 +0300

    Another test case

--- test/index.js
+++ test/index.js
@@ -48,4 +48,15 @@ describe('ArabicReshaper', function() {
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
     expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
+
+  it('respect foreign characters', function() {
+  	var a = String.fromCharCode(0x0628)+String.fromCharCode(0x0628)+String.fromCharCode(0x0628)+'a'+
+						String.fromCharCode(0x0628)+String.fromCharCode(0x0628)+'a'+
+						String.fromCharCode(0x0628),
+  			b = String.fromCharCode(0xFE91)+String.fromCharCode(0xFE92)+String.fromCharCode(0xFE90)+'a'+
+						String.fromCharCode(0xFE91)+String.fromCharCode(0xFE90)+'a'+
+						String.fromCharCode(0xFE8F);
+    expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+    expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
+  });
 });
