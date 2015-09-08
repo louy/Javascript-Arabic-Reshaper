26a2bc14a87131164bb52185333181d8d5258ff4
Louy <me@l0uy.com>
2015-09-08 23:39:40 +0300

    Run all tests in both directions

--- test/index.js
+++ test/index.js
@@ -9,16 +9,19 @@ describe('ArabicReshaper', function() {
   it('should convert an isolated letter', function() {
   	var a = 'س', b = String.fromCharCode(0xFEB1);
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+    expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
 
   it('should convert two letter', function() {
   	var a = 'سس', b = String.fromCharCode(0xFEB3)+String.fromCharCode(0xFEB2);
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+    expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
 
   it('should convert three letter', function() {
   	var a = 'سسس', b = String.fromCharCode(0xFEB3)+String.fromCharCode(0xFEB4)+String.fromCharCode(0xFEB2);
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+    expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
 
   it('should convert', function() {
