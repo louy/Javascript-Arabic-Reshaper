4cf2f02b21c02b8c4b16d75dc648926d95409a68
Louy <me@l0uy.com>
2015-09-08 23:40:06 +0300

    More specific tests

--- test/index.js
+++ test/index.js
@@ -4,8 +4,6 @@ var expect = require('chai').expect;
 describe('ArabicReshaper', function() {
   var ArabicReshaper = require('..');
 
-  var a = 'السلام عليكم', b = 'ﺍﻟﺴﻼﻡ ﻋﻠﻴﻜﻢ';
-
   it('should convert an isolated letter', function() {
   	var a = 'س', b = String.fromCharCode(0xFEB1);
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
@@ -24,11 +22,21 @@ describe('ArabicReshaper', function() {
     expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
 
-  it('should convert', function() {
+  it('should convert a word', function() {
+  	var a = 'السلام', b = 'ﺍﻟﺴﻼﻡ';
     expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+    expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
 
-  it('should convert back', function() {
+  it('should convert another word', function() {
+  	var a = 'عليكم', b = 'ﻋﻠﻴﻜﻢ';
+    expect(ArabicReshaper.convertArabic(a)).to.equal(b);
+    expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
+  });
+
+  it('should convert a sentence', function() {
+  	var a = 'السلام عليكم', b = 'ﺍﻟﺴﻼﻡ ﻋﻠﻴﻜﻢ';
+    expect(ArabicReshaper.convertArabic(a)).to.equal(b);
     expect(ArabicReshaper.convertArabicBack(b)).to.equal(a);
   });
 });
